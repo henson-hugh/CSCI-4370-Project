5338aa3b9c69f1f95b113e37c3aadde9d30a8336
henson-hugh <63986682+henson-hugh@users.noreply.github.com>
2021-11-18 00:19:57 -0500

    added material components to admin menu page

--- src/app/admin-menu/admin-menu.component.ts
+++ src/app/admin-menu/admin-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-menu',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
   }
